e0476ba30347bf9b64c254f50a47cec858e62448
RiotpKiotrep <piotr.ka@poczta.onet.pl>
2017-12-16 12:01:14 +0100

    Dodanie rąk postaci i portali

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+    public GameObject destination;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            collider.transform.position = destination.transform.position;
+        }
+    }
+}
